5cb86151c9945f3ec7e3c7abceb9869fa739004f
Administrator <Administrator@DESKTOP-26Q4TMG>
2023-12-25 17:08:28 +0530

    5 th commit

--- src/com/anushachandran1502/dungeongame/DungeonGame.java
+++ src/com/anushachandran1502/dungeongame/DungeonGame.java
@@ -9,74 +9,193 @@ public class DungeonGame {
 		private Scanner scanner=new Scanner(System.in);
 	public static void main(String[] args) {
 		dungeonGame=new DungeonGame();
-		dungeonGame.Question3();
+		dungeonGame.Question5();
 		
 	}
 
-	private void Question3() {		
+	private void Question5() {		
 		System.out.println("Enter the Rows");
 		int row=scanner.nextInt();
 		System.out.println("Enter the column");
 		int column=scanner.nextInt();
+		int[][] rooms=new int[row][column];
 		System.out.println("Position of Adverturer row");
 		int adRow=scanner.nextInt();
 		System.out.println("Position of Adverturer Column");
 		int adCol=scanner.nextInt();
-		System.out.println("Position of Monster row");
-		int mRow=scanner.nextInt();
-		System.out.println("Position of Monster Column");
-		int mCol=scanner.nextInt();
-		System.out.println("Position of tigger  row");
-		int tRow=scanner.nextInt();
-		System.out.println("Position of tigger Column");
-		int tCol=scanner.nextInt();
-		System.out.println("Position of  Gold row");
+		System.out.println("Position of Gold row");
 		int gRow=scanner.nextInt();
-		System.out.println("Position of  Gold column");
+		System.out.println("Position of Gold Column");
 		int gCol=scanner.nextInt();
-		dungeonGame.findMinSteps(row,column,adRow,adCol,mRow,mCol,tRow,tCol,gRow,gCol);
-		}
-		private void findMinSteps(int row, int column, int adRow, int adCol, int mRow, int mCol,int tRow,int tCol, int gRow, int gCol) {
-			
-			if (adRow>row || adCol>column)
-			{
-					System.out.println("Adventurer position is Limit Exceeded");
-				}
-				else if(gRow>row || gCol>column)
-				{
-					System.out.println("Gold position is Limit Exceeded");
-				}
-				else if(mRow>row || mCol>column)
-				{
-					System.out.println("Monster position is Limit Exceeded");
-				}
-				else
-				{
-					int adGreachPosition=Math.abs(adRow-gRow)+Math.abs(adCol-gCol);
-					int mGreachposition=Math.abs(mRow-gRow)+Math.abs(mCol-gCol);
-					if(mGreachposition<adGreachPosition)
-					{
-						//System.out.println("No possible solution");
-						if(tRow>row || tCol>column)
-						{
-								System.out.println("Tigger position is Limit Exceeded");
-						}
-						else
-						{
-							int adTGreachPosition=Math.abs(adRow-tRow)+Math.abs(adCol-tCol);
-							int tGreachPosition=Math.abs(tRow-gRow)+Math.abs(tCol-gCol);
-							System.out.println("Mininum number of Steps :"+(adTGreachPosition+tGreachPosition));
 		
-						}
-						
-					}
-					else
-					{
-					
-						System.out.println("Mininum number of Steps :"+adGreachPosition);
-					}
-					
+		System.out.println("Enter the number of pits");
+		int nPits=scanner.nextInt();
+		for (int i = 0; i < nPits; i++) {
+			System.out.println("Enter the Position of pit" + (i + 1));
+			int pitRow = scanner.nextInt();
+			int pitCol = scanner.nextInt();
+			if (pitRow > row || pitRow <= 0 || pitCol > column || pitCol <= 0)
+				System.out.println("Pits are not in the limit");
+			else
+				rooms[pitRow - 1][pitCol - 1] = 1;
+		}
+		if (row <= 0 || column <= 0)
+			System.out.println("Invalid only positive values Row and column");
+		else if (adRow > row || adCol > column || adRow <= 0 || adCol <= 0) {
+			System.out.println("Adventure position is limit exceeded");
+		} else if (gRow > row || gCol > column || gRow <= 0 || gCol <= 0) {
+			System.out.println("Gold position is limit exceeded");
+		}
+		else
+		{
+			dungeonGame.findMinPath(row - 1, column - 1, adRow - 1, adCol - 1, gRow - 1, gCol - 1, rooms);
+		}
+	}
+		private void findMinPath(int row, int column, int adRow, int adCol, int gRow, int gCol, int[][] rooms) {
+			if ((adRow + 1 < rooms.length && rooms[adRow + 1][adCol] == 1) && (adRow - 1 >= 0 && rooms[adRow - 1][adCol] == 1)
+					&& (adCol + 1 < rooms[0].length && rooms[adRow][adCol + 1] == 1)
+					&& (adCol - 1 >= 0 && rooms[adRow][adCol - 1] == 1))
+			{
+				System.out.println("No Possible Solution");
 				}
+			
+			else {
+				List<List<Integer>> list = new ArrayList<>();
+				if (adRow < gRow)
+					leftToRight(rooms, adRow, adCol, gRow, gCol, list);
+				else
+					rightToLeft(rooms, adRow, adCol, gRow, gCol, list);
+				
+				//System.out.println(list);
+				System.out.println("Minimum Number of Steps " + (list.size() - 1));
+			}
+		
+	}
+
+		private void rightToLeft(int[][] rooms, int adRow, int adCol, int gRow, int gCol, List<List<Integer>> list) {
+			List<Integer> li;
+			while (adRow < rooms.length) {
+				li = new ArrayList<>();
+				li.add(adRow + 1);
+				li.add(adCol + 1);
+				if (!list.contains(li))
+					list.add(li);
+				if (adRow == gCol)
+					break;
+				if (adRow < gCol) {
+					if (adRow + 1 < rooms.length && rooms[adRow + 1][adCol] != 1)
+						adRow++;
+					else if (adCol + 1 < rooms[0].length && rooms[adRow][adCol + 1] != 1)
+						adCol++;
+					else if (adCol - 1 >= 0 && rooms[adRow][adCol - 1] != 1)
+						adCol--;
+					else if(adRow-1>=0&&rooms[adRow-1][adCol]!=1)
+						adRow--;
+				}
+				if (gCol < adRow) {
+					if (adRow - 1 >= 0 && rooms[adRow - 1][adCol] != 1)
+						adRow--;
+					else if (adCol + 1 < rooms[0].length && rooms[adRow][adCol + 1] != 1)
+						adCol++;
+					else if (adCol - 1 >= 0 && rooms[adRow][adCol - 1] != 1)
+						adCol--;
+					else if(adRow+1<rooms.length&&rooms[adRow+1][adCol]!=1)
+						adRow++;
+				}
+			}
+			while (adCol < rooms[0].length) {
+				li = new ArrayList<>();
+				li.add(adRow + 1);
+				li.add(adCol + 1);
+				if (!list.contains(li))
+					list.add(li);
+				if (adCol == gRow)
+					break;
+				if (adCol < gRow) {
+					if (adCol + 1 < rooms[0].length && rooms[adRow][adCol + 1] != 1)
+						adCol++;
+					else if (adRow - 1 >= 0 && rooms[adRow - 1][adCol] != 1)
+						adRow--;
+					else if (adRow + 1 < rooms.length && rooms[adRow + 1][adCol] != 1)
+						adRow++;
+					else if(adCol-1>=0&&rooms[adRow][adCol-1]!=0)
+						adCol--;
+				}
+
+				if (gRow < adCol) {
+					if (adCol - 1 >= 0 && rooms[adRow][adCol - 1] != 1)
+						adCol--;
+					else if (adRow - 1 >= 0 && rooms[adRow - 1][adCol] != 1)
+						adRow--;
+					else if (adRow + 1 < rooms.length && rooms[adRow + 1][adCol] != 1)
+						adRow++;
+					else if(adCol+1<rooms[0].length&&rooms[adRow][adCol+1]!=1)
+						adCol++;
+				}
+			}
+
+					}
+
+		private void leftToRight(int[][] rooms, int adRow, int adCol, int gRow, int gCol, List<List<Integer>> list) {
+			List<Integer> li;
+			while (adCol < rooms[0].length) {
+				li = new ArrayList<>();
+				li.add(adRow + 1);
+				li.add(adCol + 1);
+				if (!list.contains(li))
+					list.add(li);
+				if (adCol == gCol)
+					break;
+				else if (adCol < gCol) {
+					if (adCol + 1 < rooms[0].length && rooms[adRow][adCol + 1] != 1)
+						adCol++;
+					else if (adRow - 1 >= 0 && rooms[adRow - 1][adCol] != 1)
+						adRow--;
+					else if (adRow + 1 < rooms.length && rooms[adRow + 1][adCol] != 1)
+						adRow++;
+					else if (adCol - 1 >= 0 && rooms[adRow][adCol - 1] != 1)
+						adCol--;
+				}
+
+				else {
+					if (adCol - 1 >= 0 && rooms[adRow][adCol - 1] != 1)
+						adCol--;
+					else if (adRow - 1 >= 0 && rooms[adRow - 1][adCol] != 1)
+						adRow--;
+					else if (adRow + 1 < rooms.length && rooms[adRow + 1][adCol] != 1)
+						adRow++;
+					else if (adCol + 1 < rooms[0].length && rooms[adRow][adCol + 1] != 1)
+						adCol++;
+				}
+			}
+			while (adRow < rooms.length) {
+				li = new ArrayList<>();
+				li.add(adRow + 1);
+				li.add(adCol + 1);
+				if (!list.contains(li))
+					list.add(li);
+				if (adRow == gRow)
+					break;
+				else if (adRow < gRow) {
+					if (adRow + 1 < rooms.length && rooms[adRow + 1][adCol] != 1)
+						adRow++;
+					else if (adCol + 1 < rooms[0].length && rooms[adRow][adCol + 1] != 1)
+						adCol++;
+					else if (adCol - 1 >= 0 && rooms[adRow][adCol - 1] != 1)
+						adCol--;
+					else if (adRow - 1 >= 0 && rooms[adRow - 1][adCol] != 0)
+						adRow--;
+				} else {
+					if (adRow - 1 >= 0 && rooms[adRow - 1][adCol] != 1)
+						adRow--;
+					else if (adCol + 1 < rooms[0].length && rooms[adRow][adCol + 1] != 1)
+						adCol++;
+					else if (adCol - 1 >= 0 && rooms[adRow][adCol - 1] != 1)
+						adCol--;
+					else if (adRow + 1 < rooms.length && rooms[adRow + 1][adCol] != 1)
+						adRow++;
+				}
+			}
 		}	
 }
 
